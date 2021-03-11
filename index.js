cd036ee13caaa3b581c6e93e29d479d6c0a80fc5
Aset Telmanov <50516342+NoNameTitan@users.noreply.github.com>
2021-03-11 21:14:28 +0500

    -

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-; ((g) => {
-    if (document == undefined && window == undefined && g == window) {
-        return new Error()
-    }
-
+const tools = (() => {
+    if (document == undefined && window == undefined && globalThis == window) return new Error()
+    let $$ = document
     //#region Search Document Element[s]
     /**
      * @param { string | "div" } query
      * @returns { Element | HTMLDivElement }
      */
-    function $(query) {
-        return document.querySelector(query)
-    }
+    function $(query) { return $$.querySelector(query) }
     /**
      * @param {string} id
      */
-    function $id(id) {
-        return document.getElementById(id)
-    }
+    function $id(id) { return $$.getElementById(id) }
     /**
      * @param {string} query
      */
-    function $all(query) {
-        return document.querySelectorAll(query)
-    }
+    function $all(query) { return $$.querySelectorAll(query) }
     //#endregion
 
     //#region CSS Function[s]
@@ -30,64 +22,39 @@
      * @param { HTMLElement } target
      * @param { string } css
      */
-    function contains(target, css) {
-        return target.classList.contains(css)
-    }
+    function contains(target, css) { return target.classList.contains(css) }
     /**
      * @param { HTMLElement } target
      * @param { string } css
      */
-    function add(target, css) {
-        target.classList.add(css)
-    }
+    function add(target, css) { target.classList.add(css) }
     /**
      * @param { HTMLElement } target
      * @param { string } css
      */
-    function remove(target, css) {
-        target.classList.remove(css)
-    }
+    function remove(target, css) { target.classList.remove(css) }
     /**
      * @param { HTMLElement } element
      * @param { string } css
      */
-    function toggle(target, css) {
-        let params = [target, css]
-        contains(...params) ? remove(...params) : add(...params)
-    }
+    function toggle(target, css) { let p = [target, css]; contains(...p) ? remove(...p) : add(...p) }
     /**
      * @param { HTMLDivElement } div
      * @param {{ }} style
      */
-    function Styler(div, style) {
-        for (const key in style) {
-            if (Object.hasOwnProperty.call(div.style, key)) {
-                div.style[key] = style[key]
-            }
-        }
-    }
+    function Styler(div, style) { for (const k in style) if (Object.hasOwnProperty.call(div.style, k)) div.style[k] = style[k] }
     //#endregion
 
     //#region Scroll to "target"
     /**
      * @param {HTMLElement} target
      */
-    function smooth(target) {
-        target.scrollIntoView({ behavior: "smooth" })
-    }
+    function smooth(target) { target.scrollIntoView({ behavior: "smooth" }) }
     //#endregion
 
     //#region Tools Function[s]
-    /**
-     * @param  { ...string } args
-     */
-    const print = function (...args) {
-        console.log(...args)
-    }
     const is = {
-        empty: (value) => {
-            return (value == undefined || value == null)
-        },
+        empty: (value) => (value == undefined || value == null),
         mobileCheck: () => {
             let check = false;
             (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
@@ -111,33 +78,28 @@
      * @param { string | string[] } cssClass
      */
     function Div(cssClass) {
-        let x = document.createElement("div")
-        if (typeof cssClass == "string") {
-            add(x, cssClass)
-        } else if (Array.isArray(cssClass)) {
-            x.classList.add(...cssClass)
-        }
+        let x = document.createElement("div"), y = cssClass
+        if (typeof cssClass == "string") add(x, y)
+        else if (Array.isArray(y)) x.classList.add(...y)
         return x
     }
-
-    
     //#endregion
 
 
-    // globalThis.css = {
-    //     add,
-    //     remove,
-    //     toggle,
-    //     contains,
-    //     smooth,
-    //     Styler
-    // }
-    globalThis.$ = $
-    globalThis.$all = $all
-    globalThis.$id = $id
-    globalThis.is = is
-    globalThis.$$ = document
-    globalThis.isMobile = isMobile
-    globalThis.isMobileAndTablet = isMobileAndTablet
-    console.log("connected tools")
-})(globalThis);
+
+    return {
+        add,
+        remove,
+        toggle,
+        contains,
+        smooth,
+        Styler,
+        Div,
+        $,
+        $all,
+        $id,
+        is,
+        isMobile,
+        isMobileAndTablet
+    }
+})();
