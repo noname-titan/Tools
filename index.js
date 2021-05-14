63d09a0de925ded330cc8ba0f9129f99c9ee6c1d
NoNameTitan <50516342+NoNameTitan@users.noreply.github.com>
2021-05-14 20:05:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,105 +1,252 @@
-const tools = (() => {
-    if (document == undefined && window == undefined && globalThis == window) return new Error()
-    let $$ = document
-    //#region Search Document Element[s]
-    /**
-     * @param { string | "div" } query
-     * @returns { Element | HTMLDivElement }
-     */
-    function $(query) { return $$.querySelector(query) }
-    /**
-     * @param {string} id
-     */
-    function $id(id) { return $$.getElementById(id) }
-    /**
-     * @param {string} query
-     */
-    function $all(query) { return $$.querySelectorAll(query) }
-    //#endregion
+//#region Types
+/** @typedef { number } n */
+/**
+ * @typedef { "string" | "number" | "bigint" | "boolean" |
+ * "symbol" | "undefined" | "object" | "function" } allTypes
+ */
+/** @typedef { (value, index: n) => (void | boolean)} iterator */
+/** @typedef { (value, name: string) => (void | boolean)} iteratorOBJ */
+//#endregion
 
-    //#region CSS Function[s]
-    /**
-     * @param { HTMLElement } target
-     * @param { string } css
-     */
-    function contains(target, css) { return target.classList.contains(css) }
-    /**
-     * @param { HTMLElement } target
-     * @param { string } css
-     */
-    function add(target, css) { target.classList.add(css) }
-    /**
-     * @param { HTMLElement } target
-     * @param { string } css
-     */
-    function remove(target, css) { target.classList.remove(css) }
-    /**
-     * @param { HTMLElement } element
-     * @param { string } css
-     */
-    function toggle(target, css) { let p = [target, css]; contains(...p) ? remove(...p) : add(...p) }
-    /**
-     * @param { HTMLDivElement } div
-     * @param {{ }} style
-     */
-    function Styler(div, style) { for (const k in style) if (Object.hasOwnProperty.call(div.style, k)) div.style[k] = style[k] }
-    //#endregion
+const { ToolKit, tools } = (g => {
 
-    //#region Scroll to "target"
+  //#region Check type
+  let is = Object.freeze({
     /**
-     * @param {HTMLElement} target
+     * @param {*} value 
+     * @param { allTypes } type
      */
-    function smooth(target) { target.scrollIntoView({ behavior: "smooth" }) }
-    //#endregion
+    type: (value, type) => typeof value === type,
+    func: value => is.type(value, "function"),
+    num: value => is.type(value, "number"),
+    str: value => is.type(value, "string"),
+    obj: value => is.type(value, "object"),
+    empty: value => (value == null || value == undefined),
 
-    //#region Tools Function[s]
-    const is = {
-        empty: (value) => (value == undefined || value == null),
-        mobileCheck: () => {
-            let check = false;
-            (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-            return check;
-        },
-        mobileAndTabletCheck: () => {
-            let check = false;
-            (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-            return check;
-        }
+    array: value => Array.isArray(value),
+
+    notClass: value => (value == g || is.empty(value))
+  })
+  //#endregion
+
+  //#region Each
+  /**
+   * @param { *[] } arr
+   * @param { iterator } fn
+   */
+  let each = (arr, fn) => {
+    for (let i = 0; i < arr.length; i++)
+      if (fn(arr[i], i) === true) return
+  }
+  /**
+   * @param {{ }} obj
+   * @param { iteratorOBJ } fn
+   */
+  each.obj = (obj, fn) => {
+    for (let k in obj)
+      if (Object.hasOwnProperty.call(obj, k))
+        if (fn(obj[k], k) === true) return
+  }
+  //#endregion
+
+  //#region Object Extender
+  /**
+   * @param {{ }} target
+   * @param {{ }} obj
+   */
+  let EXTEND = (target, obj) => each.obj(obj, (x, k) => target[k] = x)
+  /**
+   * @param {new { }} target
+   * @param {{}} proto
+   */
+  EXTEND.pro = (target, proto) => each.obj(proto,
+    (x, k) => target.prototype[k] = x);
+  /**
+   * @param {{ }} target
+   * @param {{ }} proto 
+   */
+  EXTEND.binder = (target, proto) => each.obj(proto,
+    (x, k) => is.func(x) ? target[k] = x.bind(target) : target[k] = x)
+  //#endregion
+
+  //#region Object Singleton
+  /** @type { string[] } */
+  let _listMono = []
+  /**
+   * Сhecks whether this item is in the list and returns the result.
+   * After checking, it is added to the list.
+   * 
+   * If the item is in the list returns "false". So it's okay.
+   * 
+   * Else "True". This means that the item is present in the list.
+   * @param { string } self <-- new.target.name
+   * @returns 
+   */
+  let Mono = self => {
+    let has = Mono.has(self)
+    if (!has) _listMono.push(self)
+    return has
+  }
+  Mono.has = self => {
+    let has = false
+    each(_listMono, x => has = (x == self))
+    return has
+  }
+  Mono.super = (self = {}) => {
+    let has = Mono.has(self)
+    if (!has) _listMono.push(self)
+    return has
+  }
+  //#endregion
+
+  //#region Path
+  /** @param  {...string } str */
+  let path = (...str) => {
+    let z = "", y = "/"
+    forEach(str, (x, i) => {
+      x = x.trim()
+      if (i > 0 && x.slice(0, 3) == "../") {
+        z = z.slice(0, z.lastIndexOf("/"))
+        x = s.slice(2)
+      }
+      if (i > 0 && x[0] !== y) x = y + x
+      if (i < str.length && x.endsWith(y)) x = x.slice(0, x.length - 1)
+      z += x
+    })
+    return z
+  }
+  //#endregion
+
+  //#region Get [Read] doc or code
+  /**
+   * @param { string } url
+   * @param { (err: null | n, res: JSON | any) => void } fn
+   */
+  let getJSON = (url, fn) => {
+    let x = new XMLHttpRequest()
+    x.open('GET', url, true)
+    x.responseType = 'json'
+    x.onload = () => fn(x.status === 200 ? null : x.status, x.response)
+    x.send()
+  }, getData = (url, fn) => getJSON(location.origin + (url[0] !== "/" ? "/" + url : url), fn)
+
+  //#endregion
+
+  //#region Calc
+  /** @type { (a: n, b: n, c: n, d: n) => n } */
+  const calcRatio = (() => {
+    let x = v => "number" == typeof v,
+      y = (...v) => v.every(u => x(u)),
+      z = (...v) => v.reduce((a, b) => a + (x(b) ? 1 : 0), 0);
+    return (a, b, c, d) => {
+      if (z(a, b, c, d) !== 3) throw new Error("Bad arguments")
+      if (y(a, c, d)) return a * d / c
+      if (y(a, b, d)) return a * d / b
+      if (y(a, b, c)) return c * b / a
+      if (y(b, c, d)) return c * b / d
     }
+  })();
+  //#endregion
+
+  //#region #### Export Tools
+  const tools = Object.freeze({
+
+    is,
+    each,
+    EXTEND,
+    Mono,
+    path,
+    getData,
+    getJSON,
+    calcRatio
+
+  })
+  //#endregion
+
+  //#region BasicKit
+  class BasicKit {
+
+    //#region Private Param
+    static #version = "v0.1"
+    #name
     //#endregion
 
-    //#region CheckMobile
-    const isMobile = is.mobileCheck()
-    const isMobileAndTablet = is.mobileAndTabletCheck()
+    /** @param { string } name */
+    constructor(name) {
+      if (Mono.super(new.target.name))
+        throw new Error("the Kit objects must be in a single instance")
+      this.#name = name
+    }
+
+    //#region Getter
+    get name() { return this.#name }
+    static get version() { return BasicKit.#version }
     //#endregion
 
-    //#region Other
+  }
+  ; new BasicKit("Name");
+  //#endregion
+
+  //#region ToolKit
+  /** ToolKit Util */
+  const usingTools = {}
+
+  class ToolKit extends BasicKit {
+
+    //#region Static Param
+    static #self = new ToolKit()
+    static #tools = tools
+    //#endregion
+
+    constructor() { super("ToolKit"); ToolKit.use(this) }
+
+    //#region Private Static Methods
+    static #eachK(fn) { each.obj(usingTools, fn) }
+    //#endregion
+
+    //#region Static Methods
+    /** @param { BasicKit } kit */
+    static use(kit) {
+      if (kit instanceof BasicKit && !ToolKit.hasKitWithName(kit.name))
+        usingTools[kit.name] = kit
+    }
+    /** @param { striing } kitName */
+    static get(kitName) {
+      return ToolKit.hasKitWithName(kitName)
+        ? usingTools[kitName]
+        : null
+    }
     /**
-     * @param { string | string[] } cssClass
+     * @returns { BasicKit[] }
      */
-    function Div(cssClass) {
-        let x = document.createElement("div"), y = cssClass
-        if (typeof cssClass == "string") add(x, y)
-        else if (Array.isArray(y)) x.classList.add(...y)
-        return x
+    static getToolNameList() {
+      let z = []
+      ToolKit.#eachK(x => z.push(x.name))
+      return z
     }
+    /** @param { string } kitName */
+    static hasKitWithName(kitName) { return !is.empty(usingTools[kitName]) }
+    /** @param { BasicKit } kit */
+    static hasKit(kit) { return !is.empty(usingTools[kit.name]) }
     //#endregion
 
+    //#region Static Getter
+    static get BasicKit() { return BasicKit }
+    static get tools() { return tools }
+    static get self() { return ToolKit.#self }
+    //#endregion
 
+    //#region Getter
+    get tools() { return tools }
+    //#endregion
 
-    return {
-        add,
-        remove,
-        toggle,
-        contains,
-        smooth,
-        Styler,
-        Div,
-        $,
-        $all,
-        $id,
-        is,
-        isMobile,
-        isMobileAndTablet
-    }
-})();
+  }
+  //#endregion
+
+  //#region Export
+  g.ToolKit = ToolKit
+  g.tools = tools
+  return { ToolKit, tools }
+  //#endregion 
+
+})(globalThis);
