1e8fda042c254436b3e92b0f8d5e45c3d9974311
NoNameTitan <50516342+x-titan@users.noreply.github.com>
2021-05-31 18:59:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { ToolKit, tools } from "./main";
-import Algorithm from "./tools";
-import DOM from "./dom";
+import { ToolKit, tools } from "./main.js";
+import Algorithm from "./tools.js";
+import DOM from "./dom.js";
 
 export default { ToolKit, tools, Algorithm, DOM }
